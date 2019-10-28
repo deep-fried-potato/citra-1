ab252d0bee77b3bde96b503a60038426cde4d0f0
Vineet-Sharma29 <vineet.s17@iiits.in>
2019-10-29 02:24:00 +0530

    modified app/components/sidebar/index.js

--- APP/app/components/sidebar/index.js
+++ APP/app/components/sidebar/index.js
@@ -1,185 +1,35 @@
 import React, {Component} from 'react';
-import {Image} from "native-base";
-import {Content, Text, List, ListItem, Icon, Container, Left, Right, Badge} from "native-base";
+import {Content, Text, List, ListItem, Icon, Container, Left, Thumbnail} from "native-base";
 import styles from './style'
-
-const drawCover = require("../../assets/drawer-cover.png")
-const drawerImage = require("../../assets/logo-kitchen-sink.png")
 
 const data = [
     {
-        name: "Anatomy",
-        route: "Anatomy",
+        name: "Bookmarks",
+        route: "userbookmarks",
         icon: "phone-portrait",
         bg: "#C5F442"
     },
     {
-        name: "Header",
-        route: "Header",
+        name: "Posts",
+        route: "posts",
         icon: "arrow-up",
         bg: "#477EEA",
-        types: "11"
     },
     {
-        name: "Footer",
-        route: "Footer",
+        name: "Activity",
+        route: "activity",
         icon: "arrow-down",
         bg: "#DA4437",
-        types: "4"
     },
     {
-        name: "Accordion",
-        route: "NHAccordion",
+        name: "Settings",
+        route: "settings",
         icon: "repeat",
         bg: "#C5F442",
-        types: "5"
     },
-    {
-        name: "Actionsheet",
-        route: "Actionsheet",
-        icon: "easel",
-        bg: "#C5F442"
-    },
-    {
-        name: "Badge",
-        route: "NHBadge",
-        icon: "notifications",
-        bg: "#4DCAE0"
-    },
-    {
-        name: "Button",
-        route: "NHButton",
-        icon: "radio-button-off",
-        bg: "#1EBC7C",
-        types: "9"
-    },
-    {
-        name: "Card",
-        route: "NHCard",
-        icon: "keypad",
-        bg: "#B89EF5",
-        types: "8"
-    },
-    {
-        name: "Check Box",
-        route: "NHCheckbox",
-        icon: "checkmark-circle",
-        bg: "#EB6B23"
-    },
-    {
-        name: "Date Picker",
-        route: "NHDatePicker",
-        icon: "calendar",
-        bg: "#EB6B23"
-    },
-    {
-        name: "Deck Swiper",
-        route: "NHDeckSwiper",
-        icon: "swap",
-        bg: "#3591FA",
-        types: "2"
-    },
-    {
-        name: "Fab",
-        route: "NHFab",
-        icon: "help-buoy",
-        bg: "#EF6092",
-        types: "2"
-    },
-    {
-        name: "Form & Inputs",
-        route: "NHForm",
-        icon: "call",
-        bg: "#EFB406",
-        types: "12"
-    },
-    {
-        name: "Icon",
-        route: "NHIcon",
-        icon: "information-circle",
-        bg: "#bfe9ea",
-        types: "4"
-    },
-    {
-        name: "Layout",
-        route: "NHLayout",
-        icon: "grid",
-        bg: "#9F897C",
-        types: "5"
-    },
-    {
-        name: "List",
-        route: "NHList",
-        icon: "lock",
-        bg: "#5DCEE2",
-        types: "8"
-    },
-    {
-        name: "ListSwipe",
-        route: "ListSwipe",
-        icon: "code-working",
-        bg: "#C5F442",
-        types: "3"
-    },
-    {
-        name: "Picker",
-        route: "NHPicker",
-        icon: "arrow-dropdown",
-        bg: "#F50C75"
-    },
-    {
-        name: "Radio",
-        route: "NHRadio",
-        icon: "radio-button-on",
-        bg: "#6FEA90"
-    },
-    {
-        name: "SearchBar",
-        route: "NHSearchbar",
-        icon: "search",
-        bg: "#29783B"
-    },
-    {
-        name: "Segment",
-        route: "Segment",
-        icon: "menu",
-        bg: "#0A2C6B",
-        types: "3"
-    },
-    {
-        name: "Spinner",
-        route: "NHSpinner",
-        icon: "navigate",
-        bg: "#BE6F50"
-    },
-    {
-        name: "Tabs",
-        route: "NHTab",
-        icon: "home",
-        bg: "#AB6AED",
-        types: "3"
-    },
-    {
-        name: "Thumbnail",
-        route: "NHThumbnail",
-        icon: "image",
-        bg: "#cc0000",
-        types: "2"
-    },
-    {
-        name: "Toast",
-        route: "NHToast",
-        icon: "albums",
-        bg: "#C5F442",
-        types: "6"
-    },
-    {
-        name: "Typography",
-        route: "NHTypography",
-        icon: "paper",
-        bg: "#48525D"
-    }
 ]
+
+const profileImage = require("../../assets/profile.png")
 
 class Sidebar extends Component {
     constructor(props) {
@@ -195,11 +45,16 @@ class Sidebar extends Component {
             <Container>
                 <Content
                     bounces={false}
-                    style={{flex: 1, backgroundColor: '#fff', top: -1}}
-                >
-                    <Image source={drawCover} style={styles.drawerCover}/>
-                    <Image square style={styles.drawerImage} source={drawerImage}/>
-
+                    style={{flex: 1, backgroundColor: '#fff', top: -1}}>
+                    <Thumbnail large source={{uri: profileImage}}/>
+                    <Text style={{fontSize:12}} >Vineet Sharma</Text>
+                    <Text style={{fontSize:6}}>Edit Profile</Text>
+                    <View
+                        style={{
+                            borderBottomColor: 'black',
+                            borderBottomWidth: 1,
+                        }}
+                    />
                     <List
                         dataArray={data}
                         renderRow={data => <ListItem
@@ -213,19 +68,6 @@ class Sidebar extends Component {
                                     style={{color: '#777', fontSize: 26, width: 30}}/>
                                 <Text style={styles.text}>{data.name}</Text>
                             </Left>
-                            {data.types &&
-                            <Right style={{flex: 1}}>
-                                <Badge
-                                    style={{
-                                        borderRadius: 3,
-                                        height: 25,
-                                        width: 72,
-                                        backgroundColor: data.bg
-                                    }}
-                                >
-                                    <Text style={styles.badgeText}>{`${data.types}`}</Text>
-                                </Badge>
-                            </Right>}
                         </ListItem>}
                     />
                 </Content>
