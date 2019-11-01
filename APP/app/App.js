1b3a1bf5af68eee57912b9b332991d3dd7eb699c
Vineet-Sharma29 <vineet.s17@iiits.in>
2019-11-01 17:33:02 +0530

    shifted routes from app.js to route.js

--- APP/app/App.js
+++ APP/app/App.js
@@ -1,12 +1,12 @@
-import React from "react"
-import AppContainer from "./config/route"
+import React from "react";
+import AppContainer from "./config/route";
 import {Root} from "native-base";
-import Footer from "./components/footer";
-import Drawer from "./components/drawer"
+import FooterNav from "./components/footerNav";
+import SideNav from "./components/sideNav";
 
 export default () =>
     <Root>
-        <Drawer/>
+        {/*<SideNav/>*/}
         <AppContainer/>
-        <Footer/>
+        <FooterNav/>
     </Root>
