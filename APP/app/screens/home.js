30cd8dc4b2e62f0310b868e153df4e87fdfb73f5
Vineet-Sharma29 <vineet.s17@iiits.in>
2019-11-01 18:38:22 +0530

    modified home.js

--- APP/app/screens/home.js
+++ APP/app/screens/home.js
@@ -1,11 +1,40 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {ScrollView, View, Text, StyleSheet} from 'react-native';
+import {Container, Button} from "native-base";
+import Ionicon from 'react-native-vector-icons/Ionicons';
+
+const styles = StyleSheet.create({
+    text: {fontSize: 16, color: 'blue', paddingLeft: 10, paddingTop: 5, fontWeight: 'bold'},
+    circleShapeView: {
+        width: 90,
+        height: 90,
+        borderRadius: 90 / 2,
+        backgroundColor: '#00BCD4',
+        margin: 10,
+        justifyContent: 'center'
+    },
+})
+
+const icons = ['md-flame', 'md-pint', 'md-water', 'md-bug']
+
 class Home extends Component {
     render() {
         return (
-            <View>
-                <Text>Hello!!! at home.</Text>
-            </View>
+            <Container>
+                <View style={{flex: 3, backgroundColor: 'red'}}></View>
+                <View style={{flex: 1}}>
+                    <Text style={styles.text}>Filter By Category</Text>
+                    <ScrollView horizontal={true}>
+                        {
+                            icons.map((object, i) => (
+                                <Button key={i} style={styles.circleShapeView}>
+                                    <Ionicon color="#fff" size={35} name={object}></Ionicon>
+                                </Button>)
+                            )
+                        }
+                    </ScrollView>
+                </View>
+            </Container>
         );
     }
 }
