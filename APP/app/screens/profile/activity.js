e983c2f58712ce5e9fc4aaca939c8656f030ad29
Vineet-Sharma29 <vineet.s17@iiits.in>
2019-11-11 21:22:37 +0530

    modified activity.js

--- APP/app/screens/profile/activity.js
+++ APP/app/screens/profile/activity.js
@@ -1,11 +1,27 @@
 import React, {Component} from 'react';
+import {Text, View, StyleSheet} from "react-native";
+import Ionicon from 'react-native-vector-icons/Ionicons';
+
+const styles = StyleSheet.create({
+    content:{flex:1, justifyContent:'center'},
+    icon: {position: 'relative', marginTop: -16},
+});
 
 class Activity extends Component {
+    static navigationOptions = {
+        title:'Citra',
+        drawerLabel:"activity",
+        drawerIcon:({tintColor})=>(
+            <Ionicon style={styles.icon} color="#fff" size={30} name="md-journal"></Ionicon>
+        )
+    }
     render() {
         return (
-            <div>
-                
-            </div>
+            <View style={styles.content}>
+                <Text>
+                    Hello activity.....
+                </Text>
+            </View>
         );
     }
 }
