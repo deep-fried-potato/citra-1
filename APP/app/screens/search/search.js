966c73b443c78a365a2b01fa242a6944117ed608
Vineet-Sharma29 <vineet.s17@iiits.in>
2019-11-11 21:26:15 +0530

    modified seach.js

--- APP/app/screens/search/search.js
+++ APP/app/screens/search/search.js
@@ -1,21 +1,13 @@
 import React, {Component} from 'react';
 import {View, Text} from "react-native";
 import {Container} from "native-base";
-import Ionicon from "react-native-vector-icons/Ionicons";
-import Banner from "../components/banner";
+import SearchInput from "./searchInput";
 
 class Search extends Component {
-    static navigationOptions = {
-        title:'Citra',
-        tabBarLabel:"Search",
-        tabBarIcon:({tintColor})=>(
-            <Ionicon color={tintColor} size={30} name="md-search"></Ionicon>
-        )
-    }
     render() {
         return (
             <Container>
-                <Banner name="Search" />
+                <SearchInput></SearchInput>
                 <View>
                     <Text>Search here !!!</Text>
                 </View>
