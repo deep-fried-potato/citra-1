fa5abb017aba4cde2d7a1b7c5d508300132d987c
Vineet-Sharma29 <vineet.s17@iiits.in>
2019-11-11 21:19:31 +0530

    modified route.js

--- APP/app/config/route.js
+++ APP/app/config/route.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import {createStackNavigator} from "react-navigation-stack";
 import {createAppContainer} from "react-navigation";
-import {createDrawerNavigator} from "react-navigation-drawer";
-import Home from "../screens/home";
+// import { createDrawerNavigator } from 'react-navigation-drawer';
+import Home from "../screens/home/home";
 import PostCreate from "../screens/postCreate";
 import PostDetail from "../screens/postDetail";
-import Search from "../screens/search";
+import Search from "../screens/search/search";
 import Notifications from "../screens/notifications";
 import Sos from "../screens/sos";
-import UserSettings from "../screens/profile/settings";
-import UserPosts from "../screens/profile/postList";
-import UserBookmarks from "../screens/profile/bookmarks";
-import UserActivity from "../screens/profile/activity";
-import UserEditProfile from "../screens/profile/editProfile";
 
-// const Drawer = createDrawerNavigator({
-//     UserBookmarks: {screen: UserBookmarks},
-//     UserPosts:{screen:UserPosts},
-//     UserSettings:{screen:UserSettings},
-//     UserActivity:{screen:UserActivity},
-//     UserEditProfile:{screen:UserEditProfile}
-// });
 
 const appNavigator = createStackNavigator({
     // Drawer:{screen:Drawer},
@@ -30,6 +18,10 @@ const appNavigator = createStackNavigator({
     PostDetail: {screen: PostDetail},
     Notifications: {screen: Notifications},
     Sos: {screen: Sos}
+}, {
+    defaultNavigationOptions: {
+        header: null
+    }
 });
 
 const AppContainer = createAppContainer(appNavigator);
