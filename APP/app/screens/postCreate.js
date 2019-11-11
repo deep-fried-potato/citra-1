5be0402a358dc5665bbd706830d312b41c41eae4
Vineet-Sharma29 <vineet.s17@iiits.in>
2019-11-11 21:21:35 +0530

    modified postCreate.js

--- APP/app/screens/postCreate.js
+++ APP/app/screens/postCreate.js
@@ -1,12 +1,44 @@
 import React, {Component} from 'react';
-import {View, Text} from "react-native";
+import {View, StyleSheet} from "react-native";
+import {Container, Header, Content, Textarea, Form, Footer, FooterTab, Button, Icon, Text} from 'native-base';
+import Banner from "../components/banner";
+
+const styles = StyleSheet.create({
+    footer:{position:'absolute' ,bottom:0}
+});
 
 class PostCreate extends Component {
     render() {
         return (
-            <View>
-                <Text>Post Create here!!!!</Text>
-            </View>
+            <Container>
+                <View>
+                    <Form>
+                        <Textarea style={{margin:10}} bordered rowSpan={3} placeholder="What issue are you facing?"></Textarea>
+                    </Form>
+                </View>
+
+                    <Footer style={styles.footer}>
+                        <FooterTab style={{backgroundColor:'white'}}>
+                            <Button vertical>
+                                <Icon name="apps" />
+                                <Text>Apps</Text>
+                            </Button>
+                            <Button vertical>
+                                <Icon name="camera" />
+                                <Text>Camera</Text>
+                            </Button>
+                            <Button vertical active>
+                                <Icon active name="navigate" />
+                                <Text>Navigate</Text>
+                            </Button>
+                            <Button vertical>
+                                <Icon name="person" />
+                                <Text>Contact</Text>
+                            </Button>
+                        </FooterTab>
+                    </Footer>
+
+            </Container>
         );
     }
 }
