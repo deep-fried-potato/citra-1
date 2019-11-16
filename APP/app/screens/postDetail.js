367a93d2c60059143098fab43ad12da968296b32
Siddhant Jain <mail.siddhantjain19@gmail.com>
2019-11-17 03:25:01 +0530

    working on detailed post

--- APP/app/screens/postDetail.js
+++ APP/app/screens/postDetail.js
@@ -1,19 +1,56 @@
 import React from 'react';
-import {} from 'react-native';
+import {AsyncStorage} from 'react-native';
 import {Text, Container} from 'native-base';
+import Carousel from 'react-native-snap-carousel';
+import axios from 'axios';
 
 class PostDetail extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            postid: this.props.navigation.getParam('postid'),
+            post: null
         }
     }
+
+    _fetchPost= async () => {
+        const userToken =  await AsyncStorage.getItem('userToken')
+        const postid =  this.props.navigation.getParam('postid')
+       console.log(postid)
+        axios.get(`http://139.59.75.22:3000/common/viewIssue/${postid}`, {
+            headers: {
+                'x-access-token': userToken,
+            }
+        })
+        // .then(response => response.json())
+        .then(res => {
+            console.log('lll', res.data)
+            this.setState({'post': res.data})
+        })
+        .catch(err => {console.log(err)})
+    }
+
+    componentDidMount = async () => {
+       await this._fetchPost();
+    }
+
+    _renderItem = ({item, index}) => {
+        return (
+            <View style={styles.slide}>
+                <Text style={styles.title}>{ item.title }</Text>
+            </View>
+        );
+    }
+
     render() {
         return( 
             <Container>
-                <Text>Post screen</Text>
-                <Text>{this.state.postid}</Text>
+                <Carousel
+                    ref={(c) => { this._carousel = c; }}
+                    data={this.state.entries}
+                    renderItem={this._renderItem}
+                    sliderWidth={sliderWidth}
+                    itemWidth={itemWidth}
+                />
             </Container>
         );
     }
