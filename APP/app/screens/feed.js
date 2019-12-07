a0bd38e268dd5462069fb78d6684f7fd19889fa1
Vineet-Sharma29 <vineet.s17@iiits.in>
2019-12-07 18:12:58 +0530

    modified app/screens/feed.js

--- APP/app/screens/feed.js
+++ APP/app/screens/feed.js
@@ -1,127 +1,182 @@
 import React from 'react';
-import {FlatList,  AsyncStorage} from 'react-native';
-import {Card, Text,CardItem, Left, Right, Icon, Button,Thumbnail, Body} from 'native-base';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import {View, FlatList, AsyncStorage, PermissionsAndroid, Image} from 'react-native';
+import {Card, Text, CardItem, Left, Right, Icon, Button, Thumbnail, Body, ActionSheet, Grid, Col} from 'native-base';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import Config from "react-native-config";
+import styles from './styles'
 
-import axios from 'axios';
-// const axios = require('axios').default;
+const uri = 'https://miro.medium.com/max/1914/1*gyr5EOhxd17hPf5SY_KXYA.jpeg';
 
+class Item extends React.Component {
 
-const DATA = [
-    {
-        id:'1',
-        title:'Why so serious?',
-        content : 'Like any other social media site Facebook has length requirements when it comes to writing on the wall.',
-        tags:['Zuora', 'Subscription', 'Startup', 'Entrepreneurship' ,'Sales' ,'Marketing' ,'Pitching']
-    },
-    {
-        id:'2',
-        title:'Why so serious?',
-        content : 'Like any other social media site Facebook has length requirements when it comes to writing on the wall.',
-        tags:['Zuora', 'Subscription', 'Startup', 'Entrepreneurship' ,'Sales' ,'Marketing' ,'Pitching']
-    },
-    {
-        id:'3',
-        title:'Why so serious?',
-        content : 'Like any other social media site Facebook has length requirements when it comes to writing on the wall.',
-        tags:['Zuora', 'Subscription', 'Startup', 'Entrepreneurship' ,'Sales' ,'Marketing' ,'Pitching']
-    },
-];
-
-function Item ({card, postpage} ) {
-    return (
-        <Card>
-                <CardItem >
-                    <Left>
-                        {/* icon for differnt tags like potholes gaerbage etc */}
-                        <Thumbnail source={require('../assets/drawer-cover.png')} />
-                        <Body>
-                            <Text>{card.tags[0]}</Text>
-                            <Text note> Posted by {card.author} | {card.time} ago </Text>
-                            {/* <Text note> {card.tags.map(tag => { `<li> #${tag}</li>` })} </Text> */}
-                        </Body>
-                    </Left> 
-                </CardItem>
-                <TouchableOpacity onPress={() => postpage(card.id)}>
-                    <CardItem cardBody>
-                        {/* <Image source={require('../assets/drawer-cover.png')}  style={{height: 200, width: null, flex:1}} /> */}
-                        <Body>
-                            <Text>{card.title}</Text>
-                            <Text note>{card.content}</Text>
-                            {/* content max 100 chars */}
-                        </Body>
-                    </CardItem>
-                </TouchableOpacity>
-                <CardItem footer>
-                    <Left>
-                        <Button transparent>
-                            <Icon active name="thumbs-up" />
-                            <Text>{card.upvotes|| 12}</Text>
-                        </Button>
-                    </Left>
-                    <Body>
-                        <Button transparent>
-                            <Icon active name="chatbubbles" />
-                            <Text>{card.countcomments || 4}</Text>
-                        </Button>
-                     </Body>
-                    <Right>
-                        <Button transparent>
-                            <Text>2.4 Km away</Text>
-                        </Button>
-                        
-                    </Right>
- 
-                </CardItem>
-        </Card>
-      );    
-}
-
-
-class feed extends React.Component{
-
-    constructor(props){
-        super(props);
+    constructor(props) {
+        super(props)
         this.state = {
-            feed : []
+            time: null,
+            distance: null,
+            upvote: false,
+            total_upvote: null,
+            options: {},
+            userId: null,
         }
     }
 
-    _postpage = (id)=>{
-        this.props.navigation.navigate('postscreen', {postid : id});
+    diff_days = (dt2, dt1) => {
+        let diff = (dt2.getTime() - dt1.getTime()) / 1000;
+        if ((diff /= 60) <= 59) {
+            return `${Math.abs(Math.round(diff))} m`
+        } // minutes
+        else if ((diff /= 60) <= 23) {
+            return `${Math.abs(Math.round(diff))} h`
+        }// hours
+        else {
+            diff /= 24;
+            return `${Math.abs(Math.round(diff))} d`
+        }
     }
 
-    fetchfeed = async () => {
-        const userToken = await AsyncStorage.getItem('userToken');
-        axios.get('http://139.59.75.22:3000/common/getIssues', {
-            params:{
-                lat: 17.399320,
-                lng: 78.521402,
-                rad: 5,
-            },
+    _parseDate = () => {
+        let time = new Date(this.props.card.addedDate);
+        let time_now = new Date();
+        this.setState({time: this.diff_days(time_now, time)});
+    }
+
+    _parseDistance = () => {
+        //TODO: Correct this function
+        let distance = Math.sqrt(Math.pow((this.props.latitude - this.props.card.location.lat), 2)
+            + Math.pow((this.props.longitude - this.props.card.location.lng), 2))
+        distance = Math.round(distance);
+        this.setState({distance});
+    }
+
+    _getUser = (id) => {
+        //TODO: /getresident API update
+        return id;
+    }
+    _upvote = () => {
+        //TODO: if this.props.card.post.upvotes has userid :
+        // console.log(this.props.card._id)
+        if (!this.state.upvote) {
+            fetch(`http://${Config.BASE_URL}:3000/resident/upvoteIssue/:${this.props.card._id}`, {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    'x-access-token': this.props.token
+                }
+            })
+                .then(response => response.json())
+                .then((resjson) => {
+                    // console.log(resjson);
+                    this.setState({'upvote': true, 'total_upvote': this.state.total_upvote + 1});
+                })
+                .catch(err => (console.log('Error', err)));
+        } else {
+            this.setState({'upvote': false, 'total_upvote': this.state.total_upvote - 1});
+        }
+    }
+
+    _deletePost = (postId) => {
+        fetch(`http://${Config.BASE_URL}:3000/resident/deleteIssue/:${postId}`, {
+            method: 'POST',
             headers: {
-                'x-access-token': userToken,
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                'x-access-token': this.props.token
             }
         })
-        .then((res) =>{
-            console.log(res);
-        })
-        .catch(err => {console.log(err)}) 
+            .then(response => response.json())
+            .then((resjson) => {
+                // this.setState({'upvote' : true, 'total_upvote': this.state.total_upvote+1});
+                console.log('Post Deleted')
+                console.log(resjson);
+            })
+            .catch(err => (console.log('Error', err)));
+    }
+
+    _getUserFromStorageAsync = async () => {
+        let userId = await AsyncStorage.getItem('userId');
+        this.setState({'userId': userId})
     }
 
     componentDidMount = () => {
-        this.fetchfeed();
+        this._parseDate();
+        this._parseDistance();
+        this._getUserFromStorageAsync();
+        this.setState({'total_upvote': this.props.card.upvotes.length})
     }
 
-    render(){
-        return(
-            <FlatList 
-                data = {DATA}
-                renderItem = {({item}) => <Item card = {item} postpage = {this._postpage} />}
-                keyExtractor = {item => item.id} 
-            /> 
+// PostDetail
+    render() {
+        let BUTTONS = [
+            {text: "Remove from feed", icon: "trash", iconColor: "#606060"},
+            // { text: "Share", icon: "analytics", iconColor: "#f42ced" },
+            // { text: "Option 2", icon: "aperture", iconColor: "#ea943b" },
+            {text: "Cancel", icon: "close", iconColor: "#25de5b"}
+        ]
+
+        // console.log(this.state.userId)
+        //TODO:Can't perform delete
+
+        if (this.props.card.addedBy === this.state.userId) {
+            BUTTONS.push({text: "Delete", icon: "trash", iconColor: "#fa213b"})
+        }
+
+        var DESTRUCTIVE_INDEX = 3;
+        var CANCEL_INDEX = 4;
+
+        return (
+            <View style={styles.card}>
+                <Card>
+                    <CardItem cardBody>
+                        <Image source={{uri: uri}} style={styles.image}/>
+                    </CardItem>
+                    <CardItem cardBody style={{paddingHorizontal:8, marginBottom:15}}>
+                        <View>
+                            <Text style={styles.caption} onPress={() => this.props.postpage(this.props.card._id)}>{this.props.card.title}</Text>
+                            <Text note style={styles.subCaption}><Icon type="Feather" name="map-pin" style={{color:'#808080', fontSize:14}}></Icon> {this.state.distance} Km</Text>
+                            <Text note style={styles.subCaption}>1hr | Anuj Aggarwal</Text>
+                            <Text style={styles.description}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur consequatur dignissimos
+                                doloremque ea iure molestias necessitatibus neque quibusdam saepe unde?</Text>
+                        </View>
+                    </CardItem>
+                    <CardItem bordered footer>
+                        <Left>
+                            <TouchableOpacity onPress={() => this._upvote()}>
+                                <Text><Icon active name="thumbs-up"/> {this.state.total_upvote}</Text>
+                            </TouchableOpacity>
+                        </Left>
+                        <Body />
+                        <Right>
+                            <TouchableOpacity
+                                onPress={() => {
+                                    ActionSheet.show(
+                                        {
+                                            options: BUTTONS,
+                                            cancelButtonIndex: CANCEL_INDEX,
+                                            destructiveButtonIndex: DESTRUCTIVE_INDEX,
+                                            title: "Select an option"
+                                        },
+                                        buttonIndex => {
+                                            if (buttonIndex == 0) {
+                                                this.props._removeFromFeed(this.props.card._id)
+                                            } else if (BUTTONS[buttonIndex] !== undefined && BUTTONS[buttonIndex]['text'] === 'Delete') {
+                                                this._deletePost(this.props.card.id);
+                                                this.props._removeFromFeed(this.props.card.id);
+                                            }
+                                        }
+                                    )
+                                }
+                                }>
+                                <Icon style={styles.icon} type="Feather" name="more-vertical"></Icon>
+                            </TouchableOpacity>
+                        </Right>
+                    </CardItem>
+                </Card>
+                <View style={styles.hr}/>
+            </View>
         );
     }
 }
 
-export default feed;
+export default Item;
