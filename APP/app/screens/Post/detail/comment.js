a48ebbe8de980748c6c60aafbacf74706ef8311d
Vineet-Sharma29 <vineet.s17@iiits.in>
2019-12-07 18:12:02 +0530

    modified app/screens/Post/detail/comment.js

--- APP/app/screens/Post/detail/comment.js
+++ APP/app/screens/Post/detail/comment.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import {FlatList,  AsyncStorage, PermissionsAndroid} from 'react-native';
-import {Card, Text,CardItem, Left, Right, Icon, Button,Thumbnail, Body, ActionSheet} from 'native-base';
+import {View, Card, Text,CardItem, Left, Right, Icon, Button,Thumbnail, Body, ActionSheet} from 'native-base';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+
+const uri =
+    'https://facebook.github.io/react-native/docs/assets/favicon.png';
+
 
 export default class Comment extends React.Component{
     constructor(props){
@@ -11,18 +15,16 @@ export default class Comment extends React.Component{
     render(){
         console.log(this.props.comment)
         return(
-            <Card style={{backgroundColor:'#797c86'}} >
-                <CardItem header>
-                    <Body>
-                        <Text>{this.props.comment.item.user}</Text>
-                    </Body>
-                </CardItem>
-                <CardItem>
-                    <Body>
-                        <Text note>{this.props.comment.item.text}</Text>
-                    </Body>
-                </CardItem>
-            </Card>
+            <View style={{marginVertical:10, flex:1, flexDirection:'row'}}>
+                <View style={{flex:0.15}}>
+                    <Thumbnail style={{alignSelf:'center'}} small source={{uri: uri}} />
+                </View>
+                <View style={{flex:0.85, paddingHorizontal:10, backgroundColor:'rgb(230,230,230)', marginRight:20, marginTop:5, borderRadius:8, borderTopLeftRadius:0}}>
+                    <Text style={{paddingTop:5, fontWeight:'bold'}}>{this.props.comment.item.user}</Text>
+                    <Text note>1 hr</Text>
+                    <Text style={{paddingVertical:10, fontWeight:'100', fontSize:14}}>{this.props.comment.item.text}</Text>
+                </View>
+            </View>
         )
     }
 }
