eb78efa5a9fadc7e0ff088af889e85caa838ada7
Vineet-Sharma29 <vineet.s17@iiits.in>
2019-12-07 18:08:47 +0530

    modified app/api/post.js

--- APP/app/api/post.js
+++ APP/app/api/post.js
@@ -1,9 +1,19 @@
 import session from './session'
+import upload from "./uploadImage";
+import {AsyncStorage} from "react-native"
+
+const headers = {
+    'Content-Type': 'application/json',
+    'x-access-token': AsyncStorage.getItem('userToken')
+};
 
 export default {
-    createIssue(metrics){
-        session.post('/resident/addIssue',{
-            ...metrics
-        })
+    createIssue(metrics) {
+        const {media, ...issue} = {...metrics}
+        const mediaUrls = []
+        media.map(
+            media => upload.uploadToS3(media).then(response => mediaUrls.push(response.body.postResponse.location))
+        );
+        session.post('/resident/addIssue', {...issue, 'media':mediaUrls}, {headers:headers})
     }
 }
